3c1014e9d4081bf67fe98fcb9e6ffe4f42bd0e2b
Ley Missailidis <moisis.langley@missailidis.ca>
2017-02-16 15:55:36 -0800

    elixir 1.4.0 parenthesis

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Akaw.Mixfile do
   use Mix.Project
 
-  def project do
+  def project() do
     [app: :akaw,
      version: "0.7.0",
      elixir: "~> 1.3",
@@ -10,11 +10,11 @@ defmodule Akaw.Mixfile do
      deps: deps]
   end
 
-  def application do
+  def application() do
     [applications: [:logger, :couchbeam]]
   end
 
-  defp deps do
+  defp deps() do
     [
       {:couchbeam, "~> 1.4"},
       {:ex_doc, "~> 0.11", only: :dev},
@@ -24,13 +24,13 @@ defmodule Akaw.Mixfile do
     ]
   end
 
-  defp description do
+  defp description() do
     """
     CouchDB client, wrapping couchbeam erlang client.
     """
   end
 
-  defp package do
+  defp package() do
     [
       files: ~w(lib mix.exs README.md LICENSE CHANGELOG.md),
       maintainers: ["Thomas Ringling"],
