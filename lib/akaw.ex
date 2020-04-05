0e5017c491b43aa2aa5fe2ec39552dd70e355cec
Ley Missailidis <moisis.langley@missailidis.ca>
2020-04-05 08:58:19 -0700

    So it begins ... again

---
+++ lib/akaw.ex
@@ -0,0 +1,18 @@
+defmodule Akaw do
+  @moduledoc """
+  Documentation for Akaw.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Akaw.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
